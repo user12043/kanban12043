d0990a380f7417182fcce6b594908cc2255089cf
user12043 <muaz.karatas@ogr.sakarya.edu.tr>
2018-07-04 17:08:42 +0300

    project initialized with intellij idea

---
+++ src/main/java/ogr/user12043/kanbanBoard/Main.java
@@ -0,0 +1,11 @@
+package ogr.user12043.kanbanBoard;
+
+/**
+ * Created by ME99735 on 04.07.2018 - 16:50
+ * Part of project: kanbanboard
+ */
+public class Main {
+    public static void main(String args[]) {
+        new MainPane();
+    }
+}
